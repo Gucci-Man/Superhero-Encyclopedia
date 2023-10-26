d89bf8865df2cdb30946a11979216d27db589acb
mrtomno <adel.ngo@yahoo.com>
2023-10-25 23:19:11 -0500

    updated user views test file

--- test_user_views.py
+++ test_user_views.py
@@ -2,7 +2,7 @@
 
 # run these tests like:
 #
-#    python3 -m unittest test_views_model.py
+#    python3 -m unittest test_user_views.py
 
 from unittest import TestCase
 from app import app, CURR_USER_KEY
@@ -62,3 +62,94 @@ class UserViewsTestCase(TestCase):
         """Clean up fouled transactions."""
 
         db.session.rollback()
+
+    def test_info_page(self):
+        """Test that the main superhero encyclopedia page shows when logged in"""
+
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+            
+            test_uid = self.u1.id
+            resp = c.get("/superheros")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<span class="cell-name">Black Widow</span>', html)
+            self.assertEqual(sess[CURR_USER_KEY], test_uid)
+
+    def test_invalid_user_page(self):
+        """Test that invalid user will be unable to access website"""
+
+        with app.test_client() as c:
+            resp = c.get("/superheros", follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            # Should redirect back to login page and show flash message
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('>Access unauthorized.</div>', html)
+
+    def test_user_profile(self):
+        """Test to view user profile when logged in"""
+        
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+
+            test_uid = self.u1.id 
+            resp = c.get(f"/users/{test_uid}")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<li>Username: test1</li>', html)
+
+    def test_user_favorites(self):
+        """Test to view user's favorites page"""
+
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+
+            
+            resp = c.get("/users/favorites")
+            html = resp.get_data(as_text=True)
+
+            # Test user favorite should be groot
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<h3>Superhero: Groot</h3>', html)
+
+    def test_add_favorite(self):
+        """Test adding a favorite superhero"""
+
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+
+            # Test hero id is Nick Fury
+            test_hid = self.hid2
+
+            resp = c.post(f"/superheros/{test_hid}/fav", follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+
+            # Test user should now have 2 favorites, originally had 1 favorite
+            favs = Favorites.query.filter(Favorites.user_id==self.u1.id).all()
+            self.assertEqual(len(favs), 2)
+            self.assertEqual(favs[1].user_id, self.u1.id)
+            self.assertEqual(favs[1].hero_id, test_hid)
+
+    def test_remove_favorite(self):
+        """Test removing a favorite superhero"""
+
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+
+            # Test hero to un-favorite is Groot, from SetUp method
+            test_hid = self.hid1   
+
+            resp = c.post(f"/superheros/{test_hid}/fav", follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+
+            # Test user should now have 0 favorites
+            favs = Favorites.query.filter(Favorites.user_id==self.u1.id).all()
+            self.assertEqual(len(favs), 0)
